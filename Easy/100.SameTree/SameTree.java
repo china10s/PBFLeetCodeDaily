3702116c4fed48464c77c718a0b71946954b9c1e
guest <420503690@qq.com>
2017-10-15 08:47:40 +0800

    Easy/100.SameTree/

---
+++ Easy/100.SameTree/SameTree.java
@@ -0,0 +1,25 @@
+
+
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+ 
+public class SamrTree{
+	public static boolean isSameTree(TreeNode node1,TreeNode node2){
+		if(node1 == null &&node2 ==null){
+			return true;
+		}
+		else if( node1 !=null && node2 !=null){
+			return (isSameTree(node1.left,node2.left) && isSameTree(node1.right,node2.right));
+		}
+		else{
+			return false;
+		}
+	}
+	public static void main(String[] args){
+
+	}
+}
