58d75c248e6761962da878f4d35eb57a82fbcf4d
guest <420503690@qq.com>
2017-10-18 17:31:21 +0800

    Hard/4.MedianOfTwoSortedArrays/

---
+++ Hard/4.MedianOfTwoSortedArays/MedianOfTwoSortedArrays.java
@@ -0,0 +1,48 @@
+import java.lang.Math;
+import java.util.Arrays;
+
+public class MedianOfTwoSortedArrays{
+	public static double MedianOfTwoSortedArrays(int[] nums1,int[] nums2){
+		int m = nums1.length;
+		int n = nums2.length;	
+		int k = m+n;
+		if(k%2==1){
+			return findKth(nums1,m,nums2,n,k/2+1);
+		}
+		else{
+			return ((findKth(nums1,m,nums2,n,k/2)+findKth(nums1,m,nums2,n,k/2+1))/2);
+		}
+	}
+	public static double findKth(int a[],int m,int[] b,int n,int k){
+		System.out.println("a:"+Arrays.toString(a));
+		System.out.println("b:"+Arrays.toString(b));
+		if (m>n){
+			return findKth(b,n,a,m,k);
+		}
+		if (m==0){
+			return b[k-1];
+		}
+		if(k==1){
+			System.out.println("a:"+a[0]);
+			System.out.println("b:"+b[0]);
+			return Math.min(a[0],b[0]);
+		}
+		int pa = Math.min(k/2,m),pb=k-pa;
+		if (a[pa-1] < b[pb-1]){
+			int[] aRes=new int[m-pa];
+			System.arraycopy(a,pa,aRes,0,m-pa);
+			return findKth(aRes,m-pa,b,n,k-pa);
+		}
+		else if (a[pa-1] > b[pb-1]){
+			int[] bRes=new int[n-pb];
+			System.arraycopy(b,pb,bRes,0,n-pb);
+			return findKth(a,m,bRes,n-pb,k-pb);
+		}
+		else{
+			return a[pa-1];
+		}
+	}
+	public static void main(String [] args){
+		System.out.println(MedianOfTwoSortedArrays(new int[] {1,3,5,7,9},new int[] {2,4,6,8,10}));
+	}
+}
